4bbee987e1f333130f3ee2dadbce2847ce45e16e
Joey J <joey.jaulimsing@live.com>
2024-03-23 09:40:35 +0400

    chore: log

--- src/retrieveHistoricalCandles.mjs
+++ src/retrieveHistoricalCandles.mjs
@@ -11,7 +11,6 @@ export default async function retrieveHistoricalCandles(symbol, timeframe, start
 
     const config = {
         headers: {
-            'Content-Type': 'application/json',
             'Accept': 'application/json',
             'auth-token': metaApiToken
         },
